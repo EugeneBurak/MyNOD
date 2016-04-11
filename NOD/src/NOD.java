252694e1b411092cffa5a4d2e56d2692927cda0b
Eugene Burak <eugene.burak@gmail.com>
2016-04-12 01:15:53 +0300

    Return comments

--- NOD/src/NOD.java
+++ NOD/src/NOD.java
@@ -18,6 +18,9 @@ public class NOD
 {
     public static void main(String[] args) throws IOException
         {
+
+//
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             int a = Integer.parseInt(reader.readLine());
             int b = Integer.parseInt(reader.readLine());
@@ -78,4 +81,91 @@ public class NOD
             }
         }
     }
+
+    /*
+    package com.javarush.test.level14.lesson08.bonus02;
+
+
+
+    public class Solution
+    {
+        public static void main(String[] args) throws IOException
+        {
+
+//    //числа Фибоначи
+//    public static void main(String[] args)
+//    {
+//        int r, n1 = 0, n2 =1;
+//        for (int n = 1; n <= 20; n++)
+//        {
+//            r = n1+n2;
+//            System.out.println(n+": "+r);
+//            n1 = n2;
+//            n2 = r;
+//        }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int a = Integer.parseInt(reader.readLine());
+            int b = Integer.parseInt(reader.readLine());
+            if (a == 1) System.out.println(a);
+            else if (b == 1) System.out.println(b);
+            else if (a == b) System.out.println(a);
+            else if (a % b == 0) System.out.println(b);
+            else if (b % a == 0) System.out.println(a);
+            else
+            {
+                ArrayList<Integer> list1 = new ArrayList<Integer>();
+                int x = a;
+                int n = 2;
+                while (x != 1)
+                {
+                    if (x % n == 0)
+                    {
+                        list1.add(n);
+                        x = x / n;
+                    } else
+                    {
+                        n = n + 1;
+                    }
+                }
+                ArrayList<Integer> list2 = new ArrayList<Integer>();
+                int y = b;
+                int m = 2;
+                while (y != 1)
+                {
+                    if (y % m == 0)
+                    {
+                        list2.add(m);
+                        y = y / m;
+                    } else
+                    {
+                        m = m + 1;
+                    }
+                }
+                ArrayList<Integer> list3 = new ArrayList<Integer>();
+                for (int n1 = 0; n1 < list1.size(); n1++)
+                {
+                    for (int m1 = 0; m1 < list2.size(); m1++)
+                    {
+                        if (list1.get(n1) == list2.get(m1))
+                        {
+                            list3.add(list1.get(n1));
+                        }
+                    }
+                }
+                if (list3.size() == 0)
+                    System.out.println(1);
+                else
+                {
+                    sort(list3);
+                    System.out.print("Наибольший общий делитель (НОД) = ");
+                    System.out.println(list3.get(list3.size() - 1));
+                }
+            }
+        }
+    }
+
+
+
+    */
 
