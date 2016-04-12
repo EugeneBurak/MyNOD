e2dc4309b81b12f4c8fcf87d2bf3917c371f919e
Eugene Burak <eugene.burak@gmail.com>
2016-04-12 11:58:21 +0300

    Initial commit

--- NOD/src/NOD.java
+++ NOD/src/NOD.java
@@ -90,18 +90,7 @@ public class NOD
     {
         public static void main(String[] args) throws IOException
         {
-
-//    //числа Фибоначи
-//    public static void main(String[] args)
-//    {
-//        int r, n1 = 0, n2 =1;
-//        for (int n = 1; n <= 20; n++)
-//        {
-//            r = n1+n2;
-//            System.out.println(n+": "+r);
-//            n1 = n2;
-//            n2 = r;
-//        }
+// я решил потренироваться!
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             int a = Integer.parseInt(reader.readLine());
